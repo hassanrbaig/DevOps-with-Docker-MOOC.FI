6d1281d14b3ed711741348bff64042d2e9d40976
Hassan Raza <hassaninside@gmail.com>
2024-06-12 19:35:52 +0500

    Create script.sh

---
+++ Part3/Exercise 3.4/script.sh
@@ -0,0 +1,12 @@
+github_repo=$1
+dockerhub_repo=$2
+
+echo "$DOCKER_PWD" | docker login --username "$DOCKER_USR" --password-stdin
+
+repo_name=$(basename "$github_repo")
+git clone https://github.com/"$github_repo".git
+cd "$repo_name"
+
+docker build -t hassanrbaig/"$repo_name" .
+
+docker push "$dockerhub_repo"
