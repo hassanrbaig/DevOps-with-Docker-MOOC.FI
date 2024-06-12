c3d371016952b37ea95e001b66b5d211c5368e9d
Hassan Raza <hassaninside@gmail.com>
2024-06-12 19:25:47 +0500

    Create script.sh

---
+++ Part3/Exercise 3.3/script.sh
@@ -0,0 +1,10 @@
+github_repo=$1
+dockerhub_repo=$2
+
+repo_name=$(basename "$github_repo")
+git clone https://github.com/"$github_repo".git
+cd "$repo_name"
+
+docker build -t ellebee/"$repo_name" .
+
+docker push "$dockerhub_repo"
